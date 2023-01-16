615a8194aa3a0a988a2ae5bd9143ea461ead57b6
parth <parth.vora@simformsolutions.com>
2023-01-16 12:05:17 +0530

    Second developer commit

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'First developer commit:',
+              'Second developer commit:',
             ),
             Text(
               '$_counter',
